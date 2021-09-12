0c8514895d7cc985eb8c928cc9a9d9da59082d43
cj1354421348 <ccc350977886@gmail>
2021-09-12 12:37:15 +0800

    1

--- src/views/detail/Detall.vue
+++ src/views/detail/Detall.vue
@@ -1,17 +1,26 @@
 <template>
 <navbar>
-<template v-slot:default>商品详情</template>
+<template v-slot:default>商品详情{{id}}</template>
 </navbar>
 </template>
 
 <script>
 import Navbar from '@/components/common/navbar/Navbar.vue';
+import { useRoute } from 'vue-router';
+import { ref } from '@vue/reactivity';
 export default {
   name:'',
+  setup(){
+    const route = useRoute();
+    let id =ref(0);
+    id.value = route.query.id;
+    return {
+    id
+   } 
+  }
+,
   data(){
-   return {
-
-   }
+   
   },
 //计算属性
 computed:{ 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,13 +2,83 @@
 <navbar>
 <template v-slot:default>图书兄弟</template>
 </navbar>
+<div class="banners">
+<img  src="@/assets/images/1.png" alt="">
+
+</div>
+<recommend :recommds="recommds"></recommend>
+<tabConControl @tabClick="tabClick" :titles="['畅销','新书','精选']"></tabConControl>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  {{temd}} <br>
+  
 </template>
 
 <script>
 import Navbar from '@/components/common/navbar/Navbar.vue';
+import Recommend from './childhome/recommend.vue';
+import tabConControl from "@/components/content/tabControl/tabConControl";
+import{getHomeAllData} from "@/network/home";
+import{onMounted,ref,reactive} from 'vue';
 export default {
 
   name:'Home',
+  setup(){
+    let temd = ref(0)
+    const recommds= ref([])
+    onMounted(()=>{
+      getHomeAllData().then(res=>{
+       recommds.value =res.data.goods.data
+      })
+    })
+   const tabClick = (index)=>{
+     temd.value = index;
+     //console.log(index)
+   }
+    return{
+      recommds,
+      temd,
+      tabClick,
+    }
+  },
   data(){
    return {
 
@@ -28,13 +98,20 @@ methods:{
 },
 //组件
   components: {
-    Navbar
-
+    Navbar,
+    Recommend,
+    tabConControl
 
   },
 }
 </script>
 
 <style scoped>
+.banners img{
+  width: 100%;
+  height: auto;
+  margin-top: 45px;
+}
 
 </style>
+ 
